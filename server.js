08363233eca1d6b11aa460c9a7a7cc46835a1fce
Ryan Schutte <ryan.schutte@civitaslearning.com>
2016-06-24 13:54:39 -0500

    tabs and minor rename

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
+var assetServer = new WebpackDevServer(webpack(config), {
+	publicPath: config.output.publicPath,
+	hot: true,
+	historyApiFallback: true
+});
 
-  console.log('Listening at http://localhost:3000/');
+assetServer.listen(3000, 'localhost', function (err, result) {
+	if (err) {
+		return console.log(err);
+	}
+
+	console.log('Listening at http://localhost:3000/');
 });
